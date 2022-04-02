79f46aa080d69ce95b4ef8daae2fedd528453fc4
furionias <82088657+furionias@users.noreply.github.com>
2022-04-02 16:38:28 +0100

    Add files via upload

---
+++ get_threads.py
@@ -0,0 +1,18 @@
+import threading
+
+
+
+class data():
+  
+  def __init__(self, objects):
+  	self.objects = {}
+
+
+  def get_threads():
+  	threads = []
+
+  	for i in range(78):
+  		objects.append(i)
+  		t = threading.Thread(target=)
+
+
